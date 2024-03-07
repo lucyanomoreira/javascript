9b08641111e515b3483eaf79aa69500968168a2e
lucyanomoreira <lucyano.c.moreira@gmail.com>
2024-03-07 13:12:40 -0300

    iniciando

---
+++ node_4ae62d76ece7d.tmp.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detran</title>
+</head>
+<body>
+     <h1>Sistema de Multas</h1> 
+    Velocidade do carro:<input type="number" name="txtvel" id="txtvel">
+    <input type="button" value="Verificar" onclick="calcular()">
+    <script>
+        var txtv = window.document.getElementById(txtvel)
+    </script>
+</body>
+</html>
